f467570479a55c189a15a066ec038352e159b95f
Cameron Lisk (A) <clisk_a@worldofwarcraft.com>
2020-07-31 16:52:51 +0000

    Code Commit

---
+++ Weekly Challenge 1/AD-Import.ps1
@@ -0,0 +1,29 @@
+Import-Module ActiveDirectory
+$csv = Import-CSV -Path C:\CSVs\Users.csv
+$OU='OU=Users,OU=WoW Users,DC=worldofwarcraft,DC=com'
+
+foreach($UserParam in $csv){
+    
+    $newUserID=@{
+        GivenName               =$UserParam.firstname
+        surName                 =$UserParam.lastname
+        Name                    =$UserParam.firstname + " " + $UserParam.lastname
+        Initials                =$UserParam.MiddleInitial
+        DisplayName             =$UserParam.DisplayName
+        Department              =$UserParam.Department
+        Title                   =$UserParam.Title
+        SamAccountName          =$UserParam.SamAccountName
+        #UserPrincipalName       ="$($UserParam.)@worldofwarcraft.com"
+        Path                    =$OU
+        Enabled                 =$true
+        ChangePasswordAtLogon   =$false
+        AccountPassword         =(ConvertTo-SecureString $UserParam.Password -AsPlainText -Force)
+    }
+    Try{
+        New-ADUser @newUserID -ErrorAction Stop 
+        Write-Host "UserID $($UserParam.SamAccountName) created!" -ForegroundColor green
+       }
+    Catch{
+        Write-Host "Could not create AD User $($UserParam.SamAccountName)." -ForegroundColor Red
+    }
+}
